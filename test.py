207464ff8824aa353ef02f40fd5a3f32b62344b9
Eric Sherman <ericandrewsherman@gmail.com>
2017-01-26 17:03:31 -0600

    rename variable

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class APITestCase(unittest.TestCase):
         raw_client_list = pd.read_json(list_url)
         self.choice = raw_client_list.sample().values[0]
         response_url = 'https://data.pa.gov/resource/vsaj-gjez.json?client_id=%d&$limit=1' % self.choice
-        self.df_init = pd.read_json(response_url, dtype=False)
+        self.from_source = pd.read_json(response_url, dtype=False)
         count_url = 'https://data.pa.gov/resource/vsaj-gjez.json?$select=count(*),max(client_id)&client_id=%d' % self.choice
         self.count = pd.read_json(count_url)
         self.response = self.client.get('/client_id/%d' % self.choice)
@@ -29,7 +29,7 @@ class APITestCase(unittest.TestCase):
         self.assertEqual(self.response.status_code, 200)
         pprint(json.loads(self.response.get_data()))
     def test_client_id_match(self):
-        self.assertEqual(json.loads(self.response.data)['client_id'], self.df_init['client_id'][0])
+        self.assertEqual(json.loads(self.response.data)['client_id'], self.from_source['client_id'][0])
     def test_client_count_match(self):
         self.assertEqual(json.loads(self.response.data)['count'], self.count['count_1'][0])
 
