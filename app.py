edf53670ec4eb522e575d90c92937a59cda9cc3c
Eric Sherman <ericandrewsherman@gmail.com>
2017-01-26 16:54:04 -0600

    code cleanup, unitttest tweak

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import pandas as pd
-from flask import Flask, jsonify, json, render_template, url_for, make_response, Response
+from flask import Flask, jsonify, json, render_template, url_for, make_response
 import folium
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 def get_stats(client_id):
+    """
+    Use Socrata API to query data, aggregate with pandas
+    """
     url = 'https://data.pa.gov/resource/vsaj-gjez.json?client_id='+str(client_id)
     df_init = pd.read_json(url, convert_dates=['inspection_date'], dtype=False)
     df = df_init.loc[:, ['client_id', 'county', 'inspection_date']]
@@ -27,54 +30,63 @@ def get_stats(client_id):
     data.columns = data.columns.droplevel(0)
     data = data.reset_index(0, drop=True)
     #order columns
-    result = data[['client_id','count','min_inspection_date', 'max_inspection_date', 'counties']]
+    result = data[['client_id', 'count', 'min_inspection_date', 'max_inspection_date', 'counties']]
     #truncate timestamps for display
-    result['min_inspection_date']=result['min_inspection_date'].dt.strftime('%m/%d/%Y')
-    result['max_inspection_date']=result['max_inspection_date'].dt.strftime('%m/%d/%Y')
-
-    #result['count']=result['count'].astype(int)
-    result_json=result.iloc[0]
-    #response=jsonify(json.loads(result_dict))
-    #return jsonify(client_id=data1['client_id'], count=data1['count'], min_inspection_date= data['min_inspection_date'], counties=data['counties'])
+    result['min_inspection_date'] = result['min_inspection_date'].dt.strftime('%m/%d/%Y')
+    result['max_inspection_date'] = result['max_inspection_date'].dt.strftime('%m/%d/%Y')
+    #cast count to int (Flask dosen't like numpy int64)
+    result['count'] = result['count'].astype(int)
+    result_json = result.iloc[0]
     return result_json
 
 def get_points(client_id):
+    """
+    retrive spatial data and some attributes for a given client
+    """
     url = 'https://data.pa.gov/resource/vsaj-gjez.json?$select=client_id,latitude,longitude,farm,client&client_id='+str(client_id)
     points = pd.read_json(url)
     return points
 
 @app.route('/client_id/')
 def list_view():
-    url='https://data.pa.gov/resource/vsaj-gjez.json?$select=client_id'
-    raw_client_list=pd.read_json(url)
+    """
+    'browseable' list of map view hyperlink for each client
+    """
+    url = 'https://data.pa.gov/resource/vsaj-gjez.json?$select=client_id'
+    raw_client_list = pd.read_json(url)
     client_list = raw_client_list.drop_duplicates().to_dict()
     return render_template('client_list.html', client_list=client_list)
 
 @app.route('/client_id/<int:client_id>')
 def detail_view(client_id):
-    stats=get_stats(client_id)
+    """
+    display summary data for a client
+    """
+    stats = get_stats(client_id)
     #return render_template('detail_view.html', detail_view= json_format)
     return  jsonify(json.loads(stats.to_json()))
 
 @app.route('/client_id/<int:client_id>/map')
 def make_map(client_id):
+    """
+    generates html of a leaflet map of client data
+    """
     raw_points = get_points(client_id)
     points = raw_points.drop_duplicates()
     client_map = folium.Map(zoom_start=12)
-    sw = (points['latitude'].min(), points['longitude'].min())
-    ne = (points['latitude'].max(), points['longitude'].max())
-    client_map.fit_bounds([sw, ne])
+    south_west_bound = (points['latitude'].min(), points['longitude'].min())
+    north_east_bound = (points['latitude'].max(), points['longitude'].max())
+    client_map.fit_bounds([south_west_bound, north_east_bound])
 
     for point in points.itertuples():
         detail_url = url_for('detail_view', client_id=point.client_id)
         html = '<embed src=http://localhost:5000%s>' % detail_url
         iframe = folium.element.IFrame(html=html, width=300, height=100)
-        popup = folium.Popup(iframe, max_width=2650)
         folium.Marker([point.latitude, point.longitude], popup=point.farm).add_to(client_map)
 
-    f = folium.element.Figure()
-    f.html.add_child(folium.element.Element("<h1>%s</h1>" % point.client))
-    f.add_child(client_map)
+    map_container = folium.element.Figure()
+    map_container.html.add_child(folium.element.Element("<h1>%s</h1>" % point.client))
+    map_container.add_child(client_map)
     html_map = client_map.save('./templates/%s_map.html' % client_id)
 
     return render_template('%s_map.html' % client_id)
--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-from app import app
 import json
 import unittest
 import pandas as pd
-import random
+from pprint import pprint
+from app import app
+
 
 class APITestCase(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         """
         fetch the list of ids and sample a random client_id
 
@@ -13,25 +15,23 @@ class APITestCase(unittest.TestCase):
         """
         #TODO these tests are really slow, lots of redundant data queries
         self.client = app.test_client(self)
-        list_url='https://data.pa.gov/resource/vsaj-gjez.json?$select=client_id'
-        raw_client_list=pd.read_json(list_url)
-        self.choice=raw_client_list.sample().values[0]
+        list_url = 'https://data.pa.gov/resource/vsaj-gjez.json?$select=client_id'
+        raw_client_list = pd.read_json(list_url)
+        self.choice = raw_client_list.sample().values[0]
         response_url = 'https://data.pa.gov/resource/vsaj-gjez.json?client_id=%d&$limit=1' % self.choice
         self.df_init = pd.read_json(response_url, dtype=False)
         count_url = 'https://data.pa.gov/resource/vsaj-gjez.json?$select=count(*),max(client_id)&client_id=%d' % self.choice
-        self.count= pd.read_json(count_url)
+        self.count = pd.read_json(count_url)
         self.response = self.client.get('/client_id/%d' % self.choice)
+        print("Fetched client_id is: " , self.choice )
 
     def test_query(self):
         self.assertEqual(self.response.status_code, 200)
+        pprint(json.loads(self.response.get_data()))
     def test_client_id_match(self):
         self.assertEqual(json.loads(self.response.data)['client_id'], self.df_init['client_id'][0])
     def test_client_count_match(self):
         self.assertEqual(json.loads(self.response.data)['count'], self.count['count_1'][0])
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
